64bb528c221f8d89e4505effc5ca3608cb451d35
Sankar K G <sankarfrompalani@gmail.com>
2025-06-22 22:23:33 +0530

    Refactor code structure for improved readability and maintainability

--- app/_components/HeroSection.js
+++ app/_components/HeroSection.js
@@ -1,14 +1,54 @@
 import React from 'react'
+import { BiSolidFoodMenu } from "react-icons/bi";
 
 const HeroSection = () => {
   return (
-    <div className='w-full'>
-      <div>
-        <img src='hero-bg-1111.png' className='w-full'/>
+    <div className=''>
+      <div className='relative w-full flex'>
+        <section className='w-3/4'>
+        <div className="bg-[url('/hero-bg-1.png')]  w-full h-screen bg-cover bg-center rounded-br-[120]">
+          <div className='pl-24'>
+            {/* Hero Title */}
+            <h1 className='text-4xl pt-[200] text-white font-extrabold py-12'>Authentic Non-Veg Delights.<br/> A Taste of Tradition.</h1>
+            <h4 className='text-white text-2xl'>Rooted in heritage, perfected with passion <br/> 
+              Alaimagal serves hearty South Indian non-veg <br/> meals with a warm, premium touch.</h4>
+
+            {/* Hero Buttons */}
+            <div className='flex space-x-8 py-14'>
+              <button className='bg-white font-semibold text-xl flex items-center gap-1 px-7 py-2 rounded-lg'>
+                View Menu <span className='text-3xl'><BiSolidFoodMenu /></span>
+              </button>
+              <button className='border-2 text-xl font-semibold border-white text-white rounded-lg py-2 px-8'>
+                Visit Us
+              </button>
+            </div>
+
+            {/* Zomato Section */}
+            <div className='flex items-center gap-2'>
+              <h1 className='text-white text-xl'>
+                Dine-In Available | Order Online on   
+              </h1>
+              <a href='https://www.zomato.com/coimbatore/alaimagal-hotels-2-gandhipuram/order' target='_blank' className='cusrsor-pointer'><img src='/zomatonew.png' alt='Zomato Logo' className='w-[100px]' /></a>
+            
+            </div>
+
+            
+
+          </div> 
+        </div>
+      </section>
+
+      <section className="w-1/4">
+        <div className="bg-[url('/hero-bg-2.png')] w-[350] h-full bg-cover bg-right">
+          Sankar
+        </div>
+        
+      </section>
       </div>
-      {/* <div>
-        <img src='' alt='Alaimagal Hotel Logo' className='w-[465px] py-8' />
-      </div> */}
+
+      <div className='absolute top-24 right-0'>
+        <img src='/hero-image.png' alt='' className='w-[850px] py-8' />
+      </div>
     </div>
   )
 }
--- app/_components/Navbar.js
+++ app/_components/Navbar.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full'>
+    <div className='fixed w-full z-40'>
         <section className='w-full flex '>
             {/* Logo */}
             <div className='w-3/4 flex justify-center items-center'>
-                <img src='/logo-main.png' alt='Alaimagal Hotel Logo' className='w-[465] py-8' />
+                <img src='/logo-main.png' alt='Alaimagal Hotel Logo' className='w-[350] py-8' />
             </div>
             
             {/* Visit Us */}
             <div className='w-1/4 flex justify-center items-start py-8'>
-                <button className='bg-white text-black px-8 py-2 rounded-md transition duration-300'>
+                <button className='bg-[#03552A] text-white px-8 py-2 rounded-md transition duration-300'>
                     Visit Us
                 </button>
             </div>
@@ -19,14 +19,14 @@ const Navbar = () => {
 
        <section className=''>
         {/* Navigation Links */}
-        <nav className='w-3/4 flex py-4'>
-            <ul className='flex px-24 font-semibold space-x-20 text-white text-2xl'>
-            <li className='relative group'>
-                <a href='#home'>Home</a>
-                <span className='absolute left-0 bottom-0 w-0 h-[4px] bg-white transition-all duration-300 group-hover:w-full'></span>
+        <nav className='w-3/4 flex'>
+            <ul className='flex pl-24 font-semibold space-x-20 text-white text-xl'>
+            <li className='relative group '>
+                <a className='hover:font-extrabold hover:text-2xl' href='#home'>Home</a>
+                <span className='absolute left-0 bottom-0 w-0 h-[4px] bg-white transition-all  duration-300 group-hover:w-full'></span>
             </li>
             <li className='relative group'>
-                <a href='#about'>Menu</a>
+                <a className='hover:font-extrabold' href='#about'>Menu</a>
                 <span className='absolute left-0 bottom-0 w-0 h-[4px] bg-white transition-all duration-300 group-hover:w-full'></span>
             </li>
             <li className='relative group'>
