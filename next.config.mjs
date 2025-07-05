4dddcc45149e6fd53013ad48e58d1bb690c1cbbb
Sankar K G <sankarfrompalani@gmail.com>
2025-07-05 14:52:09 +0530

    Add ESLint configuration to ignore warnings during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+    ignoreDuringBuilds: true, // Ignores all ESLint warnings during build
+  },
+};
 
 export default nextConfig;
