5d1d655a826206ff0ad119544ed639fc4c1663de
Sankar K G <sankarfrompalani@gmail.com>
2025-07-15 22:27:14 +0530

    Update metadata in page.js with enhanced title, description, and keywords for improved SEO

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -8,12 +8,30 @@ import Navbar from '../_components/Navbar'
 import Footer from '../_components/Footer'
 import Main from '../_components/Main'
 
- export const metadata = {
-  title: "Alaimagal Hotel",
-  icons: {
-    icon: '/favicon.ico', // or .png/.svg
+export const metadata = {
+  title: "Alaimagal Hotel Gandhipuram | Best Non-Veg Cuisine in Coimbatore",
+  description:
+    "Discover Alaimagal Hotel in Gandhipuram, Coimbatore – famous for its authentic non-vegetarian South Indian cuisine. Enjoy flavorful Chettinad dishes and homely hospitality in the heart of the city.",
+  keywords: [
+    "Alaimagal Hotel",
+    "Aalaimagal",
+    "non veg restaurant Coimbatore",
+    "Alaimagal Hotel Coimbatore",
+    "non veg restaurant Gandhipuram",
+    "best Chettinad food Coimbatore",
+    "non vegetarian hotel Gandhipuram",
+    "South Indian non veg cuisine",
+    "top restaurants in Coimbatore",
+    "budget hotels with food Gandhipuram",
+    "family dining Coimbatore",
+    "authentic Tamil Nadu food",
+
+  ],
+  alternates: {
+    canonical: "https://www.alaimagal.com", // Replace with actual URL if different
   },
-} 
+};
+
 
 
 const page = () => {
